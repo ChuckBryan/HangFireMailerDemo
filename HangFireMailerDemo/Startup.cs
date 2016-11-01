839bef6b7b0bdd74b0b9d34b401a352d578c968c
Chuck Bryan <swampyfox@gmail.com>
2016-11-01 19:49:44 -0400

    Added Owin ...

---
+++ HangFireMailerDemo/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HangFireMailerDemo.Startup))]
+
+namespace HangFireMailerDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
