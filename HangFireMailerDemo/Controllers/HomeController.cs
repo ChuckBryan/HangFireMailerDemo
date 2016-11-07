82afc1b86bd4ce7b0cbcb8dee42a5cf730a66f6e
Chuck Bryan <swampyfox@gmail.com>
2016-11-07 05:39:47 -0500

    Caught the Smtp Error so I can send back an error.

--- HangFireMailerDemo/Controllers/HomeController.cs
+++ HangFireMailerDemo/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Web.Mvc;
 
 namespace HangFireMailerDemo.Controllers
 {
+    using System;
+    using System.Net.Mail;
     using Models;
 
     public class HomeController : Controller
@@ -12,6 +14,9 @@ namespace HangFireMailerDemo.Controllers
 
         public ActionResult Index()
         {
+
+            ViewBag.ErrorMessage = TempData["ErrorMessage"];
+
             var comments = _db.MovieQuotes.OrderBy(x => x.Id).ToList();
 
             return View(comments);
@@ -23,11 +28,11 @@ namespace HangFireMailerDemo.Controllers
             // MODEL STATE VALIDATION
             if (!ModelState.IsValid) return RedirectToAction("Index");
 
-            // SAVE THE MODEL
+            // Save the Model to the Database using Entity Framework
             _db.MovieQuotes.Add(model);
             _db.SaveChanges();
 
-            // SEND THE EMAIL
+            // Send the Email Using Postal...
             var email = new NewQuoteEmail
             {
                 To = "admin@moviequotedb.com",
@@ -36,7 +41,15 @@ namespace HangFireMailerDemo.Controllers
                 Quote = model.Quote
             };
 
-            email.Send();
+            try
+            {
+                email.Send();
+            }
+            catch (SmtpException)
+            {
+                TempData["ErrorMessage"] = "I'm sorry Dave, I'm afraid I can't do that - HAL 9000, 2001: A Space Odyssey";
+            }
+            
 
             return RedirectToAction("Index");
         }
